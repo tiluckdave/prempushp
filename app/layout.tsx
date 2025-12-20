641840df5709a63137e4fbe3d0e3898e2f60d1b9
Tilak Dave <davetilak003@gmail.com>
2025-12-20 22:52:13 +0530

    new favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,9 +45,7 @@ export const metadata: Metadata = {
 			{ url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
 			{ url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
 		],
-		apple: [
-			{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-		],
+		apple: [{ url: "/apple-icon.png", sizes: "180x180", type: "image/png" }],
 		other: [
 			{ rel: "mask-icon", url: "/safari-pinned-tab.svg", color: "#1B4D2A" },
 		],
